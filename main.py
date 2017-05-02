0fe632f3f79f6eee9d1635d584c3b69d84d96b22
Danomax <sgtdano@gmail.com>
2017-05-01 23:19:38 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,87 +2,41 @@ from kivy.app import App
 from kivy.core.window import Window
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
-from kivy.uix.image import Image
-from kivy.graphics.vertex_instructions import Ellipse, Rectangle
-from kivy.graphics import Color
-from kivy.clock import Clock
 
-import random 
-	
-class Background(Image):
-	def __init__(self,**kwargs):
-		super(Background,self).__init__(**kwargs)
-		self.allow_stretch = True
-		self.keep_ratio = False
-	
-class SnakeBall(Ellipse):
-	def __init__(self,**kwargs):
-		super(SnakeBall,self).__init__(**kwargs)
-		
-class SnakeHead(SnakeBall):
-	def __init__(self,**kwargs):
-		super(SnakeHead,self).__init__(**kwargs)
+def direction(pos_ini,pos_end):
+  dx = pos_end[0]-pos_ini[0]
+  dy = pos_end[1]-pos_ini[1]
+  if dx>=0 and dx>= abs(dy):
+    direc=1
+  elif dy>=0 and dy>=abs(dx):
+    direc=2
+  elif dx<0 and abs(dx)>=abs(dy):
+    direc=-1
+  elif dy<0 and abs(dy)>=abs(dx):
+    direc=-2
+  return direc
 
-class SnakeBody(SnakeBall):
-	def __init__(self,**kwargs):
-		super(SnakeBody,self).__init__(**kwargs)
+class MyWidget(Widget):
+  def __init__(self):
+    super(MyWidget, self).__init__()
+    self.labeltext = 'direction:'
+    self.label = Label(text=self.labeltext)
+    self.add_widget(self.label)
 
-class Food(Ellipse):
-	def __init__(self,**kwargs):
-		super(Food,self).__init__(**kwargs)
-		
-		
-class Game(Widget):
-	def __init__(self):
-		super(Game, self).__init__()
-		self.score = 0
-		self.size = (320,300)
-		self.mylayout = 10
-		height_unit = int(self.height / self.mylayout)
-		self.score_label = Label(text = 'score: '+str(self.score))
-		self.add_widget(self.score_label)
-		self.score_label.height = 2*height_unit
-		#self.score_label.width = self.width
-		self.score_label.y = 8 * height_unit
-		self.background = Background(source='background.png')
-		self.add_widget(self.background)
-		self.background.height = 8*height_unit
-		self.background.width = self.width
-		self.background.y = self.y
-		
-		self.snake = []
-		self.snake_size = 3
-		self.grid = (16,12)
-		color_head = [0,0,1.0]
-		color_snake = [0.3,0.3,0.5]
-		color_food = Color([1.0,0.2,0.2])
-		(self.width_grid,self.height_grid) = (int(self.background.width/self.grid[0]),int(self.background.height/self.grid[1]))
-		with self.canvas:
-			Color(*color_head)
-			position = (self.width_grid*(self.grid[0]/2),self.height_grid*(self.grid[1]/2))
-			mysize = (self.width_grid,self.height_grid)
-			self.snake.append(SnakeHead(pos=position,size=mysize))
-			Color(*color_snake)
-			for i in range(self.snake_size-1):
-				position = (self.width_grid*(self.grid[0]/2-i-1),self.height_grid*(self.grid[1]/2))
-				self.snake.append(SnakeBall(pos=position,size=mysize))
-			#color = color_food
-			#food_pos = (random.randint(0,self.grid[0]),random.randint(0,self.grid[1]))
-			#self.food = Food(pos=food_pos,size=(self.width_grid/2,self.height_grid/2))
-		
-		Clock.schedule_interval(self.update, 1.0/60.0)
-		
-		
-		
-	def update(self,*ignore):
-		pass
+  def on_touch_down(self, touch): 
+    self.pos_ini =(touch.x, touch.y)
+    #touch.ud['dir'] = pos_ini 
+  
+  def on_touch_up(self, touch): 
+    self.pos_end = (touch.x, touch.y)
+    direc = direction(self.pos_ini,self.pos_end)
+    self.labeltext = 'direction: '+str(direc)
+    self.label.text = self.labeltext
 
+class DirTestApp(App):
+  def build(self):
+    game = MyWidget()
+    return game
 
-class Culebra2DApp(App):
-	def build(self):
-		game = Game()
-		Window.size = game.size
-		return game
-
-if __name__ == "__main__":
-	Culebra2DApp().run()
+if __name__ == '__main__':
+  DirTestApp().run()
